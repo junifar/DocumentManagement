10d397b9d84176cf6fc3b473f96111b25e92d835
junifar <junifar@gmail.com>
2013-06-12 21:53:25 +0700

    New Files

---
+++ Config/Schema/schema_0.php
@@ -0,0 +1,13 @@
+<?php 
+class DocumentManagementsSchema extends CakeSchema {
+
+	public $file = 'schema_0.php';
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+}
---
+++ Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'DocumentManagement',
+	);
+}
